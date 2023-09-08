8606bfd52a61742d6d370c1f9b5d760a45034f06
Araf Karsh Hamid <araf.karsh@metamagic.in>
2023-09-08 12:32:24 +0530

    Log Optimizations

--- src/main/java/io/fusion/air/microservice/adapters/aop/TimeTrackerAspect.java
+++ src/main/java/io/fusion/air/microservice/adapters/aop/TimeTrackerAspect.java
@@ -77,45 +77,6 @@ public class TimeTrackerAspect {
     }
 
     /**
-     * Capture Overall Method Execution Time For Controllers
-     * @param joinPoint
-     * @return
-     * @throws Throwable
-     */
-    /**
-    @Around(value = "execution(* io.fusion.air.microservice.adapters.controllers.exploits.*.*(..))")
-    public Object timeTrackerRestFixed(ProceedingJoinPoint joinPoint) throws Throwable {
-        return trackTime("WS1", joinPoint);
-    }
-    */
-
-    /**
-     * Capture Overall Method Execution Time For Controllers
-     * @param joinPoint
-     * @return
-     * @throws Throwable
-     */
-    /**
-    @Around(value = "execution(* io.fusion.air.microservice.adapters.controllers.open.*.*(..))")
-    public Object timeTrackerRestOpen(ProceedingJoinPoint joinPoint) throws Throwable {
-        return trackTime("WS2", joinPoint);
-    }
-    */
-
-    /**
-     * Capture Overall Method Execution Time for Secured Controllers
-     * @param joinPoint
-     * @return
-     * @throws Throwable
-     */
-    /**
-    @Around(value = "execution(* io.fusion.air.microservice.adapters.controllers.secured.*.*(..))")
-    public Object timeTrackerRestSecured(ProceedingJoinPoint joinPoint) throws Throwable {
-        return trackTime("WS3", joinPoint);
-    }
-    */
-
-    /**
      * Capture Overall Method Execution Time for Business Services
      * @param joinPoint
      * @return
